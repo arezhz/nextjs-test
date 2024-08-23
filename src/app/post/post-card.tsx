46cce7077ea0abe256671056a82be62882c305b6
arezhz <arezhz@gmail.com>
2024-08-24 00:27:33 +0330

    add link tag for post article

--- src/app/post/post-card.tsx
+++ src/app/post/post-card.tsx
@@ -1,13 +1,16 @@
+import Link from "next/link";
 import { IPostCardProps } from "./models/i-post-card.props";
 
 export default function PostCard({ data }: IPostCardProps) {
   return (
-    <article className=" flex flex-col justify-between p-2 w-full min-h-40 bg-white rounded-2xl border border-solid border-slate-200 shadow">
-      <div>
-        <h3 className="text-black">{data.title}</h3>
-        <p className="text-gray-500 text-xs line-clamp-2">{data.body}</p>
-      </div>
-      <span className="text-xs text-gray-400 mt-4">{data.name}</span>
+    <article className=" p-2 w-full bg-white rounded-2xl border border-solid border-slate-200 shadow">
+      <Link href={`post/${data.id}`} className="flex flex-col justify-between min-h-40">
+        <div>
+          <h3 className="text-black">{data.title}</h3>
+          <p className="text-gray-500 text-xs line-clamp-2">{data.body}</p>
+        </div>
+        <span className="text-xs text-gray-400 mt-4">{data.name}</span>
+      </Link>
     </article>
   );
 }
